bedaebe959a9fcc25aa93af2fe569898937290ec
심재헌 <simu1689@gmail.com>
2020-06-30 11:45:55 +0900

    update

--- public/plugin/plugin.js
+++ public/plugin/plugin.js
@@ -7,12 +7,12 @@
         var style = document.createElement('link');
         style.rel  = 'stylesheet';
         style.type = 'text/css';
-        style.href = 'http://3.34.185.70/chatterbox.css';
+        style.href = 'http://3.34.185.70/plugin/chatterbox.css';
         style.media = 'all';
         document.head.appendChild(style);
 
         var script = document.createElement('script');
-        script.src = 'http://3.34.185.70/bundle.4e484d7af01d20baae00.js';
+        script.src = 'http://3.34.185.70/plugin/bundle.4e484d7af01d20baae00.js';
         document.head.appendChild(script);
     }
 })();
