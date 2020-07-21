3bbe6bc24b7b487440df8dbe5d62e2dc5543e68f
심재헌 <simu1689@gmail.com>
2020-07-21 15:00:07 +0900

    prod update

--- public/plugin/plugin.js
+++ public/plugin/plugin.js
@@ -1,18 +1,27 @@
-(function() {
-    if (!document.getElementById('chatterbox-root')) {
-        var node = document.createElement('div');
-        node.id = 'chatterbox-root';
-        document.body.appendChild(node);
-
-        var style = document.createElement('link');
-        style.rel  = 'stylesheet';
-        style.type = 'text/css';
-        style.href = 'http://3.34.185.70/plugin/chatterbox.css';
-        style.media = 'all';
-        document.head.appendChild(style);
-
-        var script = document.createElement('script');
-        script.src = 'http://3.34.185.70/plugin/bundle.3624cd496987f26a3c2a.js';
-        document.head.appendChild(script);
-    }
-})();
+var _SMARTLOG_CHAT = _SMARTLOG_CHAT || (function() {
+    var _smartlog_chat_args = {}; // private
+    return {
+        init: function(Args) {
+            _smartlog_chat_args = Args
+        },
+        start: function() {
+            if (!document.getElementById('chatterbox-root')) {
+                var node = document.createElement('div');
+                node.id = 'chatterbox-root';
+                node.setAttribute('key', _smartlog_chat_args.key);
+                document.body.appendChild(node);
+        
+                var style = document.createElement('link');
+                style.rel  = 'stylesheet';
+                style.type = 'text/css';
+                style.href = 'https://chat.smlog.co.kr/plugin/chatterbox.css';
+                style.media = 'all';
+                document.head.appendChild(style);
+        
+                var script = document.createElement('script');
+                script.src = 'https://chat.smlog.co.kr/plugin/bundle.3624cd496987f26a3c2a.js';
+                document.head.appendChild(script);
+            }
+        }
+    }    
+}());
