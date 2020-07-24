fb7ce681b706be532ef0d16f3bc1ab345d53ba9b
심재헌 <simu1689@gmail.com>
2020-07-24 10:19:43 +0900

    Update plugin.js

--- public/plugin/plugin.js
+++ public/plugin/plugin.js
@@ -9,6 +9,8 @@ var _SMARTLOG_CHAT = _SMARTLOG_CHAT || (function() {
                 var node = document.createElement('div');
                 node.id = 'chatterbox-root';
                 node.setAttribute('key', _smartlog_chat_args.key);
+                node.setAttribute('ck', _smartlog_chat_args.key);
+                node.setAttribute('muid', _smartlog_chat_args.key);
                 document.body.appendChild(node);
         
                 var style = document.createElement('link');
