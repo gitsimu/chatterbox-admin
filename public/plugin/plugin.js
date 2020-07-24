f92e03fb4bac744ce2e86fc06eb68b75c1186d03
심재헌 <simu1689@gmail.com>
2020-07-24 14:07:46 +0900

    Update plugin.js

--- public/plugin/plugin.js
+++ public/plugin/plugin.js
@@ -10,12 +10,12 @@
         var style = document.createElement('link');
         style.rel  = 'stylesheet';
         style.type = 'text/css';
-        style.href = 'http://3.34.185.70/plugin/chatterbox.css';
+        style.href = 'http://chat.smlog.co.kr/plugin/chatterbox.css';
         style.media = 'all';
         document.head.appendChild(style);
 
         var script = document.createElement('script');
-        script.src = 'http://3.34.185.70/plugin/bundle.3624cd496987f26a3c2a.js';
+        script.src = 'https://chat.smlog.co.kr/plugin/bundle.3624cd496987f26a3c2a.js';
         document.head.appendChild(script);
     }
 })();
