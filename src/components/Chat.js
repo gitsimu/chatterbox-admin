21d9843d8e1c6e3bd2ced91d714d3364cf3841e2
심재헌 <simu1689@gmail.com>
2020-08-06 13:18:47 +0900

    Update Chat.js

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -39,55 +39,52 @@ const Chat = ({ users, settings, messagesAll, addMessages, initMessages, deleteM
 
     if (!isInit) CONNECTIONS[userid].ref.off()
 
-    const chat = database.ref(`/${key}/messages/${userid}`)
-                         .orderByChild('timestamp')
-    const page = CONNECTIONS[userid]
-                 ? CONNECTIONS[userid].page + 1
-                 : 1
+    const chat = database.ref(`/${key}/messages/${userid}`).orderByChild('timestamp')
+    const page = CONNECTIONS[userid] 
+                ? CONNECTIONS[userid].page + 1
+                : 1
 
     Promise.resolve()
-           .then(() => isLoading(true))
+      .then(() => isLoading(true))
 
-           // 기존 child_add 이벤트 off
-           .then(()=> {
-             if (!CONNECTIONS[userid]) return
+      // 기존 child_add 이벤트 off
+      .then(()=> {
+        if (!CONNECTIONS[userid]) return
+        CONNECTIONS[userid].ref.off()
+      })
 
-             CONNECTIONS[userid].ref.off()
-           })
+      // message list 가져오기
+      .then(() => {
+        return chat.limitToLast(PAGE_SIZE * page).once('value')
+      })
 
-           // message list 가져오기
-           .then(() => {
-             return chat.limitToLast(PAGE_SIZE * page)
-                        .once('value')
-           })
+      // store에 저장
+      .then((snapshots) => {
+        const arr = []
+        snapshots.forEach(snapshot => {
+          arr.push(snapshot.val())
+        })
 
-           // store에 저장
-           .then((snapshots) => {
-             const arr = []
-             snapshots.forEach(snapshot => {
-               arr.push(snapshot.val())
-             })
+        initMessages({ key: userid, value: arr })
 
-             initMessages({ key: userid, value: arr })
+        const lastMessage = arr[arr.length - 1]
+        return lastMessage.timestamp
+      })
 
-             const lastMessage = arr[arr.length - 1]
-             return lastMessage.timestamp
-           })
+      // child_add 이벤트 on
+      .then((lastTimestamp) => {
+        const ref =  chat.startAt(lastTimestamp + 1)
+        ref.on('child_added', (snapshot) => {
+          const value = snapshot.val()
+          addMessages({ key: userid, value: value })
+        })
 
-           // child_add 이벤트 on
-           .then((lastTimestamp) => {
-             const ref =  chat.startAt(lastTimestamp + 1)
-             ref.on('child_added', (snapshot) => {
-                          const value = snapshot.val()
-                          addMessages({ key: userid, value: value })
-                        })
-
-             return ref
-           })
-           .then((ref) => {
-             CONNECTIONS[userid] = { ref: ref, page: page }
-             isLoading(false)
-           })
+        return ref
+      })
+      .then((ref) => {
+        CONNECTIONS[userid] = { ref: ref, page: page }
+        isLoading(false)
+      })
   }, [database, key, userid, addMessages, initMessages])
 
   const sendMessage = React.useCallback((key, id, message, type, database) => {
@@ -143,19 +140,17 @@ const Chat = ({ users, settings, messagesAll, addMessages, initMessages, deleteM
     formData.append('key', key)
 
     return axios.post('/api/upload', formData, config)
-                .then(res => {
-                  if (res.data.result === 'success') {
-                    sendMessage(key, userid, JSON.stringify(res.data.file), 2, database)
-                    // console.log('upload-success', res)
-                  }
-                })
-                .catch(err => {
-                  if (err) {
-                    // console.log('upload-failure', err)
-                    throw err
-                  }
-                })
-                .finally(()=> {isLoading(false)})
+      .then(res => {
+        if (res.data.result === 'success') {
+          sendMessage(key, userid, JSON.stringify(res.data.file), 2, database)
+        }
+      })
+      .catch(err => {
+        if (err) {
+          throw err
+        }
+      })
+      .finally(()=> {isLoading(false)})
   }, [database, key, sendMessage, userid])
 
   const handleFileInputClear = (e) => {
@@ -265,144 +260,144 @@ const Chat = ({ users, settings, messagesAll, addMessages, initMessages, deleteM
   }, [clearMessages])
 
   return (
-      <>
-        <div className='messages card' ref={body}>
-          {/* 이전 메세지 (paging) */}
-          {CONNECTIONS[userid]
-           && messages
-           && messages.length >= PAGE_SIZE * CONNECTIONS[userid].page
-           && (
-               <div className="more-button">
-                 <div onClick={()=> paging()}>
-                   <i className="icon-arrow-up"></i>
-                   이전 메세지
-                 </div>
-               </div>
-           )}
-          {/* 최하단으로 스크롤 */}
-          {messages
-           && scrollTop >= 100
-           && (
-               <div className="scroll-bottom-button" onClick={scrollToBottom}>
-                 <div>
-                   <i className="icon-arrow-down"></i>
-                 </div>
-               </div>
-           )}
-          {messages && messages.map((m, i) => {
-            return <ChatMessage
-                opponent={userid}
-                target={target}
-                key={m.id}
-                onloadImage={scrollToBottom}
-                prev={messages[i - 1]}
-                next={messages[i + 1]}
-                {...m}
-                {...props}/>
-          })
-          }
-          <div id='file-drop-layer' className={ fileDropLayer ? 'file-drop-layer active' : 'file-drop-layer' }>
+    <>
+      <div className='messages card' ref={body}>
+        {/* 이전 메세지 (paging) */}
+        {CONNECTIONS[userid]
+        && messages
+        && messages.length >= PAGE_SIZE * CONNECTIONS[userid].page
+        && (
+          <div className="more-button">
+            <div onClick={()=> paging()}>
+              <i className="icon-arrow-up"></i>
+              이전 메세지
+            </div>
+          </div>
+         )}
+        {/* 최하단으로 스크롤 */}
+        {messages
+        && scrollTop >= 100
+        && (
+          <div className="scroll-bottom-button" onClick={scrollToBottom}>
             <div>
-              <i className='icon-cloud-upload'></i>
-              <div>여기에 파일을 드래그하면</div>
-              <div>바로 업로드됩니다.</div>
+              <i className="icon-arrow-down"></i>
             </div>
           </div>
-        </div>
-        <div className='message-form'>
-          <EmojiContainer
-              getState={emojiContainer}
-              setState={showEmojiContainer}
-              selectEmoji={selectEmoji}/>
-          <form ref={node => form = node} onSubmit={e => {
-            e.preventDefault()
-            if (!input.current.value.trim()) return
-
-            sendMessage(key, userid, input.current.value, 1, database)
-            input.current.value = ''
-          }}>
-            <div className='message-addon'>
-              <label>
-                <i className='icon-paper-clip'></i>
-                <input type='file'
-                       onClick={e => handleFileInputClear(e)}
-                       onChange={e => handleFileInput(e)}/>
-              </label>
-              <label>
-                <i className='icon-emotsmile'
-                   onClick={e => handleEmojiContainer(e)}></i>
-              </label>
-            </div>
-            <textarea
-                ref={input}
-                className='message-input'
-                placeholder='메세지를 입력해주세요.'
-                onKeyPress={(e) => {
-                  if (e.key === 'Enter' && !e.shiftKey) {
-                    e.preventDefault()
-                    form.dispatchEvent(new Event('submit'))
-                  }
-                }}/>
-            <button className='message-button-send' type='submit'>
-              <i className='icon-paper-plane'></i>
-            </button>
-            <div className='message-button-more'
-                 onClick={() => {
-                   showOptionDialog(!optionDialog)
-                 }}>
-              <i className='icon-options-vertical'></i>
-            </div>
-          </form>
-
-          <div className={optionDialog ? 'message-option-dialog' : 'message-option-dialog hide'}>
-            {target.value.state !== 2 && (
-                <div className='message-option-complete'
-                     onClick={() => {
-                       database.ref(`/${key}/users/${userid}`).update({ state: 2 })
-                       // setTabState(2)
-                       showOptionDialog(false)
-                       showInfoDialog(true)
-                       alert('이 대화가 종료처리 되었습니다.')
-                     }}>
-                  <i className='icon-power'></i>대화 종료하기
-                </div>
-            )}
-            <div className='message-option-delete'
-                 onClick={() => {
-                   /* firebase */
-                   database.ref(`/${key}/messages/${userid}`).remove()
-                   database.ref(`/${key}/users/${userid}`).remove()
-                   /* redux store */
-                   deleteMessages({ key: userid })
-                   selectedUser({})
-                   /* connections */
-                   CONNECTIONS[userid].ref.off()
-                   delete CONNECTIONS[userid]
-
-                   alert('이 대화가 삭제처리 되었습니다.')
-                 }}>
-              <i className='icon-trash'></i>대화 삭제하기
-            </div>
-          </div>
-
-          { infoDialog && (
-              <div className='dialog info'>
-                <div className='dialog-header'>
-                  <i className='icon-exclamation'></i>
-                  <span>Infomation</span>
-                </div>
-                <div className='dialog-body'>
-                  <div>이 대화는 이미 종료된 대화입니다.</div>
-                  <div>메세지를 보내면 다시 활성화됩니다.</div>
-                </div>
-              </div>
-          )}
-        </div>
-
-        { loading && (
-            <div id='loading'><div></div></div>
         )}
-      </>
+        {messages && messages.map((m, i) => {
+          return <ChatMessage
+            opponent={userid}
+            target={target}
+            key={m.id}
+            onloadImage={scrollToBottom}
+            prev={messages[i - 1]}
+            next={messages[i + 1]}
+            {...m}
+            {...props}/>
+          })
+        }
+        <div id='file-drop-layer' className={ fileDropLayer ? 'file-drop-layer active' : 'file-drop-layer' }>
+          <div>
+            <i className='icon-cloud-upload'></i>
+            <div>여기에 파일을 드래그하면</div>
+            <div>바로 업로드됩니다.</div>
+          </div>
+        </div>
+      </div>
+      <div className='message-form'>
+        <EmojiContainer
+            getState={emojiContainer}
+            setState={showEmojiContainer}
+            selectEmoji={selectEmoji}/>
+        <form ref={node => form = node} onSubmit={e => {
+          e.preventDefault()
+          if (!input.current.value.trim()) return
+
+          sendMessage(key, userid, input.current.value, 1, database)
+          input.current.value = ''
+        }}>
+          <div className='message-addon'>
+            <label>
+              <i className='icon-paper-clip'></i>
+              <input type='file'
+                onClick={e => handleFileInputClear(e)}
+                onChange={e => handleFileInput(e)}/>
+            </label>
+            <label>
+              <i className='icon-emotsmile'
+                 onClick={e => handleEmojiContainer(e)}></i>
+            </label>
+          </div>
+          <textarea
+            ref={input}
+            className='message-input'
+            placeholder='메세지를 입력해주세요.'
+            onKeyPress={(e) => {
+              if (e.key === 'Enter' && !e.shiftKey) {
+                e.preventDefault()
+                form.dispatchEvent(new Event('submit'))
+              }
+            }}/>
+          <button className='message-button-send' type='submit'>
+            <i className='icon-paper-plane'></i>
+          </button>
+          <div className='message-button-more'
+            onClick={() => {
+              showOptionDialog(!optionDialog)
+            }}>
+            <i className='icon-options-vertical'></i>
+          </div>
+        </form>
+
+        <div className={optionDialog ? 'message-option-dialog' : 'message-option-dialog hide'}>
+          {target.value.state !== 2 && (
+            <div className='message-option-complete'
+              onClick={() => {
+                database.ref(`/${key}/users/${userid}`).update({ state: 2 })
+                // setTabState(2)
+                showOptionDialog(false)
+                showInfoDialog(true)
+                alert('이 대화가 종료처리 되었습니다.')
+              }}>
+              <i className='icon-power'></i>대화 종료하기
+            </div>
+          )}
+          <div className='message-option-delete'
+            onClick={() => {
+              /* firebase */
+              database.ref(`/${key}/messages/${userid}`).remove()
+              database.ref(`/${key}/users/${userid}`).remove()
+              /* redux store */
+              deleteMessages({ key: userid })
+              selectedUser({})
+              /* connections */
+              CONNECTIONS[userid].ref.off()
+              delete CONNECTIONS[userid]
+
+              alert('이 대화가 삭제처리 되었습니다.')
+            }}>
+            <i className='icon-trash'></i>대화 삭제하기
+          </div>
+        </div>
+
+        { infoDialog && (
+          <div className='dialog info'>
+            <div className='dialog-header'>
+              <i className='icon-exclamation'></i>
+              <span>Infomation</span>
+            </div>
+            <div className='dialog-body'>
+              <div>이 대화는 이미 종료된 대화입니다.</div>
+              <div>메세지를 보내면 다시 활성화됩니다.</div>
+            </div>
+          </div>
+        )}
+      </div>
+
+      { loading && (
+        <div id='loading'><div></div></div>
+      )}
+    </>
   )
 }
 
