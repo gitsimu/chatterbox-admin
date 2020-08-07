f9b79c2eedd230bbac99ee4df656aaf024897a19
심재헌 <simu1689@gmail.com>
2020-08-07 14:52:59 +0900

    채팅 아이콘 설정 및 채팅 사용량 체크 배포

--- public/plugin/plugin.js
+++ public/plugin/plugin.js
@@ -7,6 +7,19 @@
         node.setAttribute('muid', smtgs_chat_data.muid);
         node.setAttribute('ip', smtgs_chat_data.ip);
         node.setAttribute('svid', smtgs_chat_data.svid);
+
+        // icon config
+        node.setAttribute('themeColor', smtgs_chat_data.themeColor);
+        node.setAttribute('position', smtgs_chat_data.position);
+        node.setAttribute('pc-hide', smtgs_chat_data.pc.hide);
+        node.setAttribute('pc-axisX', smtgs_chat_data.pc.axisX);
+        node.setAttribute('pc-axisY', smtgs_chat_data.pc.axisY);
+        node.setAttribute('pc-size', smtgs_chat_data.pc.size);
+        node.setAttribute('mobile-hide', smtgs_chat_data.mobile.hide);
+        node.setAttribute('mobile-axisX', smtgs_chat_data.mobile.axisX);
+        node.setAttribute('mobile-axisY', smtgs_chat_data.mobile.axisY);
+        node.setAttribute('mobile-size', smtgs_chat_data.mobile.size);
+
         document.body.appendChild(node);
 
         var style = document.createElement('link');
