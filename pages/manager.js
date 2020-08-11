2c5eee0f34da95aa08d410b87c2191487084f894
심재헌 <simu1689@gmail.com>
2020-08-11 11:08:09 +0900

    채팅관리 페이지 업데이트

--- pages/manager.js
+++ pages/manager.js
@@ -6,8 +6,17 @@ import FirebaseConfig from '../firebase.config'
 const publicIp = require('public-ip')
 const Manager = ({ settings, ...props }) => {
   const [managers, setManagers] = React.useState([])
+  const [detail, setDetail] = React.useState({})
+  const [focusUser, setFocusUser] = React.useState(null)
 
   React.useEffect(() => {
+    // simpleline icons
+    let simplelineLink = document.createElement("link")
+    simplelineLink.href = "https://cdnjs.cloudflare.com/ajax/libs/simple-line-icons/2.4.1/css/simple-line-icons.min.css"
+    simplelineLink.rel = "stylesheet"
+    simplelineLink.type = "text/css"
+    document.querySelector('body').appendChild(simplelineLink)
+
     Promise.resolve()
       .then(() => {
         return publicIp.v4()
@@ -22,6 +31,7 @@ const Manager = ({ settings, ...props }) => {
         return axios.post('https://smlog.co.kr/api/manage.php', formData, config)
       })
       .then(({data}) => {
+        setManagers([])
         fetchData(data.data)
       })
       .catch(err => {
@@ -39,9 +49,10 @@ const Manager = ({ settings, ...props }) => {
         }
 
         // https://firebase.google.com/docs/database/rest/retrieve-data?hl=ko#shallow
-        await axios.get(`${FirebaseConfig.databaseURL}/${m.chat_id}/messages.json?shallow=true&print=pretty'`)
+        await axios.get(`${FirebaseConfig.databaseURL}/${m.chat_id}/messages.json?shallow=true&print=pretty&timeout=5s'`)
           .then(({data}) => {
             if (data) {
+              console.log('data', data)
               setManagerData({id: m.chat_id, svid: m.svid, messages: data})
             }
           })
@@ -56,6 +67,28 @@ const Manager = ({ settings, ...props }) => {
     setManagers(m => [...m, newData])
   }
 
+  const setDetailData = (id, user, data) => {
+    setDetail(m => {
+      m[id] = m[id] ? {...m[id], [user]: data} : {[user]: data}
+      return {...m}
+    })
+  }
+
+  const fetchDetailData = async (id, res) => {
+    for (const m of res) {
+      await axios.get(`${FirebaseConfig.databaseURL}/${id}/messages/${m}.json?shallow=true&print=pretty&timeout=5s'`)
+        .then(({data}) => {
+          if (data) {
+            console.log('detail', data)
+            setDetailData(id, m, data)
+          }
+        })
+        .catch((err) => {
+          console.log('err', err)
+        })
+    }
+  }
+
   return (
     <div className="managers-container">
       <div>
@@ -63,21 +96,70 @@ const Manager = ({ settings, ...props }) => {
           <div className="managers-no">No.</div>
           <div className="managers-id">채팅 ID</div>
           <div className="managers-svid">SVID</div>
-          <div className="managers-message-count">보낸 메세지 수</div>
+          <div className="managers-message-count">연결된 채팅 수</div>
         </div>
         {managers && managers.map((m, i) => {
           return (
-            <div className="managers" key={i}>
-              <div className="managers-no">{i+1}</div>
-              <div className="managers-id">{m.id}</div>
-              <div className="managers-svid">{m.svid}</div>
-              <div className="managers-message-count">{Object.keys(m.messages).length}</div>
-            </div>
+            <>
+            <div className={m.id === focusUser ? "item active" : "item"} key={i}>
+              <div className="managers" onClick={() => {
+                const focus = focusUser === m.id ? null : m.id
+                setFocusUser(focus)
+                fetchDetailData(m.id, Object.keys(m.messages))
+              }}>
+                <div className="managers-no">{i+1}</div>
+                <div className="managers-id">{m.id}</div>
+                <div className="managers-svid">{m.svid}</div>
+                <div className="managers-connection-count">{Object.keys(m.messages).length}</div>
+              </div>
+              
+              {focusUser === m.id && detail[m.id] && (
+                <div className="user-list">
+                  {/* <div className="user thead">
+                    <div className="user-no">no.</div>
+                    <div className="user-id">대상</div>
+                    <div className="user-message-count">메세지 수</div>
+                  </div> */}
+                  {Object.keys(detail[m.id]).map((d, i) => {
+                    const value = detail[m.id][d]
+                    return (
+                      <div className="user" key={d}>
+                        <div className="user-no">-</div>
+                        <div className="user-id">{d}</div>
+                        <div className="user-message-count">
+                          <i className="icon-envelope"></i>
+                          <span>{Object.keys(value).length}</span>
+                        </div>
+                      </div>
+                    )
+                  })}
+                </div>
+              )}
+              </div>
+
+            {/* <div class="user-list">
+              {detail && detail.map((d, i) => {
+                if (d.id !== m.id) return
+                return (
+                  <div className="user" key={d.user}>
+                    <div className="user-no">{i+1}</div>
+                    <div className="user-id">{d.user}</div>
+                    <div className="user-message-count">{Object.keys(d.messages).length}</div>
+                  </div>
+                )
+              })}
+            </div> */}
+            </>
           )
         })}
       </div>
     </div>
   )
+}
+
+const getFirebaseAuthToken = async (uuid) => {
+  const res = await axios.post('/api/auth', { uuid: uuid })
+  return await res
 }
 
 const mapStateToProps = state => ({
