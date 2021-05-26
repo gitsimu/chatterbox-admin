a7d0968f42bc1dd77ef4053b4753b393c8a7da46
심재헌 <simu1689@gmail.com>
2021-05-26 10:54:41 +0900

    dev mode

--- public/plugin/plugin.js
+++ public/plugin/plugin.js
@@ -11,8 +11,28 @@
         style.media = 'all';
         document.head.appendChild(style);
 
+        var chatterboxScriptUrl = 'https://chat.smlog.co.kr/plugin/bundle.3624cd496987f26a3c2a.js';
+        var query = _smartlog_find_query()
+        if (query.dev == 'true') {
+            chatterboxScriptUrl = 'https://chat.smlog.co.kr/plugin/bundle.dev.js';
+        }
         var script = document.createElement('script');
-        script.src = 'https://chat.smlog.co.kr/plugin/bundle.3624cd496987f26a3c2a.js';
+        script.src = chatterboxScriptUrl;
         document.head.appendChild(script);
+    }
+
+    function _smartlog_find_query(url) {
+        var qs = url ? url : document.location.search;
+        qs = qs.split('+').join(' ');
+
+        var params = {},
+            tokens,
+            re = /[?&]?([^=]+)=([^&]*)/g;
+
+        while (tokens = re.exec(qs)) {
+            params[decodeURIComponent(tokens[1])] = decodeURIComponent(tokens[2]);
+        }
+
+        return params;
     }
 })();
