75736a6185cad786354b4cd82e09256f706896bb
JeongYuseong <j_6367@naver.com>
2021-09-08 10:07:23 +0900

    서버 체크 페이지 ssr로 변경, 로깅 추가

--- pages/check.js
+++ pages/check.js
@@ -1,3 +1,17 @@
-export default function Check() {
+export default function Check () {
   return (<div>SMLOG_CHAT_SERVER_OK</div>)
+}
+
+export async function getServerSideProps (context) {
+  const { req } = context
+  const ip = req.headers['x-real-ip'] || req.connection.remoteAddress
+  const now = new Date()
+  const nowStr = `${now.getFullYear()}-${now.getMonth()
+                                         + 1}-${now.getDate()} ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`
+
+  console.log(`[${nowStr}] CHECK ${ip}`)
+
+  return {
+    props: {} // will be passed to the page component as props
+  }
 }
